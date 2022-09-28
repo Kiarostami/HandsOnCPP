6cc2f7c99bd54d8b1cbfa9260e01438e156fe1ed
Sina Kiarostami <48089001+Kiarostami@users.noreply.github.com>
2022-09-29 00:59:05 +0300

    Update rangeSumBST.cpp

--- rangeSumBST.cpp
+++ rangeSumBST.cpp
@@ -41,7 +41,7 @@ public:
         return sum;
     }
     */
-    void traverseTree(TreeNode* root, int low, int high, int &sum)
+    void traverseTree(TreeNode* root, int &low, int &high, int &sum)
     {
         if(root == NULL) return;
         if((root->val >= low) && (root->val <= high)) sum += root->val;
