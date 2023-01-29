eaa2788231e20c9f4146e46d7d7a9eb1270bfebd
Sina Kiarostami <48089001+Kiarostami@users.noreply.github.com>
2023-01-29 23:31:13 +0200

    Update add3Numbers.cpp

--- add3Numbers.cpp
+++ add3Numbers.cpp
@@ -3,14 +3,14 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
-
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int a,b,c;
+    
     cin >> a >> b >> c;
-    //c = a + b;
     cout << a + b + c;
+    
     return 0;
 }
